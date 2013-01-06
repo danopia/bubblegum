6f238e23836244fd6421e64761cf42ac3260a83e
Daniel Lamando <me@danopia.net>
2013-01-06 15:18:09 -0500

    Remove filler from the welcome dialog

--- welcomeDialog.js
+++ welcomeDialog.js
@@ -11,17 +11,30 @@ exports.pages = [
      "protocols with the menu below the dialog."],
     ["Or, to learn more about Bubblegum, try the menu below this dialog."],
     ["Go ahead, I don't bite."]]},
-  {"name":"goals",
+  /*{"name":"goals",
    "title":"goals",
    "fields":[
     ["username", "user"],
     ["email address", "email"],
     ["password", "pass", {"type":"password"}],
-    ["password (again)", "passconf", {"type":"password"}]]},
+    ["password (again)", "passconf", {"type":"password"}]]},*/
   {"name":"protocols",
    "title":"protocols",
    "fields":[
-    ["username", "user"]]}];
+    ["Bubblegum is being designed with IRC, Facebook, and Google in mind. " +
+     "IRC is the main focus, though."],
+    ["As the project matures, I expect to be adding many more protocols. " +
+     "Some possible candidates include AIM, MSN, Yahoo, and Jabber."]]},
+  {"name":"contact",
+   "title":"contact",
+   "fields":[
+    ["Daniel Lamando is the primary developer behind the Bubblegum project. " +
+     "There are many ways to reach him:"],
+    ["danopia, on irc.tenthbit.net #offtopic"],
+    ["dan@danopia.net"],
+    ["http://danopia.net/"],
+    ["https://facebook.com/daniel.lamando"],
+    ["Or find my number and text me, if you like a challenge."]]}];
 
 exports.onSubmit = function (client, e) {
   switch (e.page) {
